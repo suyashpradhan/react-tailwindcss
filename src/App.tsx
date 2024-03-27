a3af14be3398194cb653fe842833b1f1bd78a2b1
suyashpradhan <pradhansuyash39@gmail.com>
2024-03-27 20:06:32 +0530

    chores: adds packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import Header from "./components/Header";
+import ProductCategories from "./components/ProductCategories";
+import ProductPromo from "./components/ProductPromo";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <ProductPromo />
+      <ProductCategories />
+    </>
+  );
 }
 
 export default App;
